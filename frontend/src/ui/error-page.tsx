dc01dc4274503e64edbba032ba2ea0355fdd92e0
sinskiy <sinskiy@proton.me>
2024-11-01 14:15:12 +0300

    feat: improve error page design

--- frontend/src/ui/error-page.tsx
+++ frontend/src/ui/error-page.tsx
@@ -5,21 +5,23 @@ import { Link } from "wouter";
 
 interface ErrorPageProps {
   children?: string;
-  errorCode?: number;
+  error?: number | string;
+  showBack?: boolean;
 }
 
 export default function ErrorPage({
   children = "Page not found",
-  errorCode = 404,
+  error = 404,
+  showBack = true,
 }: ErrorPageProps) {
   return (
     <section className={cn([atomics["centered-section"], classes.section])}>
-      <h1 className={classes.h1}>{errorCode}</h1>
+      <h1 className={classes.h1}>{error}</h1>
       <p className={classes.p}>{children}</p>
       <nav className={classes.nav}>
         <Link href="/">home</Link>
         <Link href="/" onClick={() => history.back()}>
-          go back
+          {showBack && "go back"}
         </Link>
       </nav>
     </section>
