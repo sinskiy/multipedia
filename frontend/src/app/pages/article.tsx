a663594a6cd6d1b64cb06807d11df6d880ee8341
sinskiy <sinskiy@proton.me>
2024-11-10 19:27:24 +0300

    feat: show styled markdown

--- frontend/src/app/pages/article.tsx
+++ frontend/src/app/pages/article.tsx
@@ -5,6 +5,7 @@ import { useParams } from "wouter";
 import ErrorPage from "../../ui/error-page";
 import Markdown from "react-markdown";
 import classes from "./article.module.css";
+import remarkGfm from "remark-gfm";
 
 export default function Article() {
   const { username, topic } = useParams();
@@ -33,14 +34,31 @@ export default function Article() {
     queryFn: () => fetchQuery(`/articles?${query}`),
   });
 
+  console.log(data);
+
   switch (status) {
     case "error":
       return <ErrorPage error={error.name}>{error.message}</ErrorPage>;
     case "pending":
       return <p>loading...</p>;
     case "success":
-      return (
-        <Markdown className={classes.article}>{data.data[0].body}</Markdown>
-      );
+      if ("error" in data) {
+        console.log(data);
+        return (
+          <ErrorPage error={data.error.status}>{data.error.message}</ErrorPage>
+        );
+      } else {
+        return (
+          <div className={classes.article}>
+            <h1>{data.data[0].topic.title}</h1>
+            <Markdown
+              components={{ h1: "h2", h2: "h3", h3: "h4", h4: "h5", h5: "h6" }}
+              remarkPlugins={[remarkGfm]}
+            >
+              {data.data[0].body}
+            </Markdown>
+          </div>
+        );
+      }
   }
 }
